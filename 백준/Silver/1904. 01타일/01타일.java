47f1bca2ffd40b23caa27e4d304069cdee58949e
조유진 <102661547+ohcuy@users.noreply.github.com>
2025-03-17 21:47:10 +0900

    [Silver III] Title: 01타일, Time: 112 ms, Memory: 18100 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+        }
+        return dp[n];
+    }
+}
