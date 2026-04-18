8efc20dc079a640a1597e09e03ae440adbd08a20
Yujin Cho <102661547+ohcuy@users.noreply.github.com>
2026-04-18 17:32:04 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -0,0 +1,9 @@
+def solution(s1, s2):
+    count = 0
+    
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            if s1[i] == s2[j]:
+                count += 1
+    
+    return count
